3b8c82c4f59fe1b9871cc6640b8ee5a597e13f4a
simon.frygnegard <simon.frygnegard@elev.it-gymnasiet.se>
2018-01-10 14:15:58 +0100

    Jag har ändrat så Rövarspråket fungerar
    // Simon F

--- rovare.py
+++ rovare.py
@@ -1,6 +1,15 @@
 def rovare(msg):
-    pass
-    #TODO write code  to translate swedish to rovarelanguage
+    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r",
+                  "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M",
+                  "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"]
+    outstring = ""
+    for char in msg:
+        if char in consonants:
+            outstring += char + "o" + char.lower()
+        else:
+            outstring += char
+
+    return outstring
 
 
 def swede(msg):
